947ea47b7b47dfbae8326fca63f8e86977dc2dc2
Docena-Shakira <shakiramarie.docene@cit.edu>
2026-04-26 23:32:21 +0800

    add constructor, getters, and setter

--- HollowAscent/src/main/java/model/Tile.java
+++ HollowAscent/src/main/java/model/Tile.java
@@ -1,5 +1,23 @@
-public class Tile{
-/
+public class Tile {
     private boolean walkable;
-    private String type;git 
+    private String type;
+
+    public Tile(boolean walkable, String type) {
+        this.walkable = walkable;
+        this.type = type;
+    }
+
+    public boolean isWalkable() { 
+
+        return walkable; 
+    }
+
+    public String getType() { 
+        return type; 
+    }
+
+    public void setWalkable(boolean walkable) { 
+        
+        this.walkable = walkable; 
+    }
 }
