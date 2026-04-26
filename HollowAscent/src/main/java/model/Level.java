e79f3d094c9aecfae50931baffc97f0dbe4a3b43
Docena-Shakira <shakiramarie.docene@cit.edu>
2026-04-26 23:35:19 +0800

    add constructor, getters, and getTile method

--- HollowAscent/src/main/java/model/Level.java
+++ HollowAscent/src/main/java/model/Level.java
@@ -1,7 +1,42 @@
+import java.util.List;
+
 public class Level{
 
     private Title[][] grid;
     private List<Button> buttons;
     private List<Door> doors;
     private Goal goal;
+
+    public Level(Tile[][] grid, List<Button> buttons, List<Door> doors, Goal goal) {
+        this.grid = grid;
+        this.buttons = buttons;
+        this.doors = doors;
+        this.goal = goal;
+
+    }
+
+    public Tile[][] getGrid() { 
+        
+        return grid; 
+    }
+    
+    public List<Button> getButtons() { 
+        
+        return buttons; 
+    }
+
+    public List<Door> getDoors() { 
+        return doors; 
+    }
+
+    public Goal getGoal() { 
+        
+        return goal; 
+    }
+    
+    public Tile getTile(int x, int y) { 
+        
+        return grid[y][x]; 
+    
+    }
 }
