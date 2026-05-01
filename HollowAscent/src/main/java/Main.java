0103bb4311ad22cdb10d1f992d8dfdd4a0be6464
Luke Brynson Larawan Cañeda <lukelarawan@gmail.com>
2026-05-01 21:58:54 +0800

    Implement main method to start the game

--- HollowAscent/src/main/java/Main.java
+++ HollowAscent/src/main/java/Main.java
@@ -1,1 +1,9 @@
-
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            GameFrame frame = new GameFrame();
+            GameEngine engine = new GameEngine(frame);
+            engine.startGame();
+        });
+    }
+}
