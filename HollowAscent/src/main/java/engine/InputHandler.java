3c2fdf2b10f4c074ad49545f7cf96c063d5550cf
Luke Brynson Larawan Cañeda <lukelarawan@gmail.com>
2026-05-10 06:22:53 -0700

    feat(InputHandler): add WASD support and debug logging to InputHandler

--- HollowAscent/src/main/java/engine/InputHandler.java
+++ HollowAscent/src/main/java/engine/InputHandler.java
@@ -1,23 +1,75 @@
+package engine;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import model.ActionType;
+
 public class InputHandler implements KeyListener {
     private ActionType lastAction;
 
     public InputHandler() {
+
         this.lastAction = null;
     }
 
-    public ActionType getLastAction() { return lastAction; }
-    public void clearAction() { lastAction = null; }
+    public ActionType getLastAction() {
+
+        return lastAction;
+    }
+
+    public void clearAction() {
+
+        lastAction = null;
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
+
+        System.out.println("Key pressed: " + e.getKeyCode());
+
         switch (e.getKeyCode()) {
-            case KeyEvent.VK_UP:    lastAction = ActionType.UP; break;
-            case KeyEvent.VK_DOWN:  lastAction = ActionType.DOWN; break;
-            case KeyEvent.VK_LEFT:  lastAction = ActionType.LEFT; break;
-            case KeyEvent.VK_RIGHT: lastAction = ActionType.RIGHT; break;
+
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_W:
+
+                System.out.println("UP/W pressed");
+                lastAction = ActionType.UP;
+                break;
+
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_S:
+
+                System.out.println("DOWN/S pressed");
+                lastAction = ActionType.DOWN;
+                break;
+
+            case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_A:
+
+                System.out.println("LEFT/A pressed");
+                lastAction = ActionType.LEFT;
+                break;
+
+            case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_D:
+
+                System.out.println("RIGHT/D pressed");
+                lastAction = ActionType.RIGHT;
+                break;
+                
+            default:
+                System.out.println("Other key: " + e.getKeyCode());
+                break;
         }
     }
 
-    @Override public void keyReleased(KeyEvent e) {}
-    @Override public void keyTyped(KeyEvent e) {}
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
 }
