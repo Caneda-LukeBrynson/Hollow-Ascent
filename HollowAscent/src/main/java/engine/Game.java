df66b31c1f1d4e68c862e8208834eb6480b5dd3d
Luke Brynson Larawan Cañeda <lukelarawan@gmail.com>
2026-05-10 06:17:23 -0700

    feat(Game): add tickCounter, CollisionManager field, and new getters and setters

--- HollowAscent/src/main/java/engine/Game.java
+++ HollowAscent/src/main/java/engine/Game.java
@@ -1,3 +1,9 @@
+package engine;
+
+import model.entity.Player;
+import model.entity.Shadow;
+import model.Level;
+
 public class Game {
 
     private Player player;
@@ -6,48 +12,87 @@ public class Game {
     private int currentLevelIndex;
     private boolean gameOver;
     private boolean levelComplete;
+    private CollisionManager collisionManager;
+    private int tickCounter;
 
     public Game(Player player, Shadow shadow) {
+
         this.player = player;
         this.shadow = shadow;
         this.currentLevelIndex = 0;
         this.gameOver = false;
         this.levelComplete = false;
+        this.collisionManager = new CollisionManager();
+        this.tickCounter = 0;
     }
 
     public Player getPlayer() {
+
         return player;
     }
 
     public Shadow getShadow() {
+
         return shadow;
     }
 
     public Level getCurrentLevel() {
+
         return currentLevel;
     }
 
     public int getCurrentLevelIndex() {
+
         return currentLevelIndex;
     }
 
     public boolean isGameOver() {
+
         return gameOver;
     }
 
     public boolean isLevelComplete() {
+
         return levelComplete;
     }
 
     public void setCurrentLevel(Level level) {
+
         this.currentLevel = level;
     }
 
     public void setGameOver(boolean gameOver) {
+
         this.gameOver = gameOver;
     }
 
     public void setLevelComplete(boolean levelComplete) {
+
         this.levelComplete = levelComplete;
+    }
+
+    public CollisionManager getCollisionManager() {
+
+        return collisionManager;
+    }
+
+    public void setCurrentLevelIndex(int index) {
+
+        this.currentLevelIndex = index;
+    }
+
+    public int getTickCounter() {
+
+        return tickCounter;
+    }
+
+    public void incrementTick() {
+
+        tickCounter++;
+    }
+
+    public void resetTickCounter() {
+        
+        tickCounter = 0;
     }
 }
