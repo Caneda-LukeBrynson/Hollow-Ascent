c9820364445228e3be356db72a67ff0cf02db9be
Docena-Shakira <shakiramarie.docene@cit.edu>
2026-05-10 22:09:35 +0800

    feat(Level): add playerSpawn field, findPlayerSpawn(), and getPlayerSpawn() to Level class

--- HollowAscent/src/main/java/model/Level.java
+++ HollowAscent/src/main/java/model/Level.java
@@ -1,56 +1,82 @@
+package model;
+
+import model.object.Button;
+import model.object.Door;
+import model.object.Ladder;
+import model.object.Goal;
+import model.Position;
 import java.util.List;
 
-public class Level{
-
-    private Tile[][] grid; //changes were made here  
+public class Level {
+    private Tile[][] grid;
     private List<Button> buttons;
     private List<Door> doors;
     private List<Ladder> ladders;
     private Goal goal;
+    private Position playerSpawn;
 
     public Level(Tile[][] grid, List<Button> buttons, List<Ladder> ladders, List<Door> doors, Goal goal) {
         this.grid = grid;
         this.buttons = buttons;
-        this.doors = doors;
         this.ladders = ladders;
+        this.doors = doors;
         this.goal = goal;
-
-    } //changes were made here
-
-    public Tile[][] getGrid() { 
-        
-        return grid; 
-    }
-    
-    public List<Button> getButtons() { 
-        
-        return buttons; 
+        this.playerSpawn = findPlayerSpawn(grid);
     }
 
-    public List<Door> getDoors() { 
-        return doors; 
+    private Position findPlayerSpawn(Tile[][] grid) {
+
+        for (int y = 0; y < grid.length; y++) {
+
+            for (int x = 0; x < grid[y].length; x++) {
+
+                if (grid[y][x].getType().equals("PLAYER_START")) {
+
+                    return new Position(x, y);
+                }
+            }
+        }
+
+        return new Position(1, 1);
     }
 
-    public List<Ladder> getLadders(){
+    public Tile[][] getGrid() {
+
+        return grid;
+    }
+
+    public List<Button> getButtons() {
+
+        return buttons;
+    }
+
+    public List<Door> getDoors() {
+
+        return doors;
+    }
+
+    public List<Ladder> getLadders() {
 
         return ladders;
     }
 
-    public Goal getGoal() { 
-        
-        return goal; 
+    public Goal getGoal() {
+
+        return goal;
     }
-    
-    public Tile getTile(int x, int y) { 
-        
-        return grid[y][x]; 
-    
+
+    public Position getPlayerSpawn() {
+
+        return playerSpawn;
+    }
+
+    public Tile getTile(int x, int y) {
+
+        return grid[y][x];
     }
 
     public boolean isGoalReached(Position pos) {
-
-        return goal.isReached(pos);
+        
+        return goal != null && goal.isReached(pos);
     }
-
-    
 }
