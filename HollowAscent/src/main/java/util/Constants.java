93724e094a0bc9919b21026c24ddb8728b4366f3
Christian Alegam <estanalegam@gmail.com>
2026-05-10 23:18:20 +0800

    feat(Constants): add runtime screen resolution detection and update game constants

--- HollowAscent/src/main/java/util/Constants.java
+++ HollowAscent/src/main/java/util/Constants.java
@@ -1,37 +1,40 @@
-// Constants.java
+package util;
+
+import java.awt.GraphicsEnvironment;
+import java.awt.GraphicsDevice;
+
 public class Constants {
-    public static final int TILE_SIZE = 64;
     public static final int GRID_ROWS = 10;
     public static final int GRID_COLS = 10;
-    public static final int SHADOW_DELAY = 3;
+    public static final int SHADOW_DELAY = 2;
     public static final int MAX_LEVELS = 5;
-    public static final int WINDOW_WIDTH = 640;
-    public static final int WINDOW_HEIGHT = 640;
-}
+    public static final int FPS = 60;
+    public static final int TICK_DELAY = 1000 / FPS;
+    public static final String GAME_TITLE = "Hollow Ascent";
 
-// Position.java (in util)
-public class Position {
-    private int x;
-    private int y;
-}
+    public static final int SCREEN_WIDTH;
+    public static final int SCREEN_HEIGHT;
+    public static final int TILE_SIZE;
+    public static final int GAME_AREA_SIZE;
+    public static final int GAME_OFFSET_X;
 
-// CollisionManager.java
-public class CollisionManager {
-    public boolean isWalkable(Level level, Position pos) {}
-}
+    public static final int WINDOW_WIDTH;
+    public static final int WINDOW_HEIGHT;
 
-// InputHandler.java
-public class InputHandler {
-    public ActionType getAction(KeyEvent e) {}
-}
+    static {
 
-// GameFrame.java (in ui)
-public class GameFrame extends JFrame {
-    public GameFrame() {}
-}
+        GraphicsDevice gd = GraphicsEnvironment
+                .getLocalGraphicsEnvironment()
+                .getDefaultScreenDevice();
+                
+        SCREEN_WIDTH  = gd.getDisplayMode().getWidth();
+        SCREEN_HEIGHT = gd.getDisplayMode().getHeight();
 
-// GamePanel.java (in ui)
-public class GamePanel extends JPanel {
-    public void render(Game game) {}
-    protected void paintComponent(Graphics g) {}
+        TILE_SIZE     = SCREEN_HEIGHT / GRID_ROWS;
+        GAME_AREA_SIZE = TILE_SIZE * GRID_ROWS;
+        GAME_OFFSET_X  = (SCREEN_WIDTH - GAME_AREA_SIZE) / 2;
+
+        WINDOW_WIDTH  = SCREEN_WIDTH;
+        WINDOW_HEIGHT = SCREEN_HEIGHT;
+    }
 }
