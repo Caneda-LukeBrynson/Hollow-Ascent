8c8d4b1477f8f0ec159ed9514d164aa5102a1a4c
Docena-Shakira <shakiramarie.docene@cit.edu>
2026-05-10 22:10:43 +0800

    feat(Shadow): add active state, startPosition, reset(), setActionsToReplay(), and update() to Shadow class

--- HollowAscent/src/main/java/model/entity/Shadow.java
+++ HollowAscent/src/main/java/model/entity/Shadow.java
@@ -1,28 +1,112 @@
-public class Shadow extends Entity {
+package model.entity;
+
+import model.ActionType;
+import model.PlayerAction;
+import model.Position;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shadow {
+    private Position position;
+    private Position startPosition;
     private List<PlayerAction> actionsToReplay;
     private int delayTicks;
     private int currentIndex;
+    private boolean active;
+    private int lastPlayerActionCount;
 
     public Shadow(Position position, int delayTicks) {
-        super(position);
+
+        this.startPosition = new Position(position.getX(), position.getY());
+        this.position = new Position(position.getX(), position.getY());
         this.actionsToReplay = new ArrayList<>();
         this.delayTicks = delayTicks;
         this.currentIndex = 0;
+        this.active = false;
+        this.lastPlayerActionCount = 0;
     }
 
-    @Override
+    public Position getPosition() {
+
+        return position;
+    }
+
+    public void setPosition(Position position) {
+
+        this.position = position;
+    }
+
     public void move(ActionType action) {
-        // logic to be added in Week 2
+
+        switch (action) {
+
+            case UP:    position = new Position(position.getX(), position.getY() - 1); break;
+            case DOWN:  position = new Position(position.getX(), position.getY() + 1); break;
+            case LEFT:  position = new Position(position.getX() - 1, position.getY()); break;
+            case RIGHT: position = new Position(position.getX() + 1, position.getY()); break;
+            default: break;
+
+        }
     }
 
-    public void update() {
-        // replay logic to be added in Week 2
+    public void update(int currentPlayerActionCount) {
+
+        if (!active) return;
+
+        if (currentPlayerActionCount <= lastPlayerActionCount) return;
+
+        if (currentIndex < delayTicks && currentIndex < actionsToReplay.size()) {
+
+            PlayerAction action = actionsToReplay.get(currentIndex);
+
+            move(action.getType());
+            currentIndex++;
+            lastPlayerActionCount = currentPlayerActionCount;
+
+        }
     }
 
     public void setActionsToReplay(List<PlayerAction> actions) {
-        this.actionsToReplay = actions;
+
+        this.actionsToReplay = new ArrayList<>(actions);
+
+        if (actions.size() >= delayTicks && !active) {
+
+            active = true;
+            position = new Position(startPosition.getX(), startPosition.getY());
+            currentIndex = 0;
+            lastPlayerActionCount = actions.size() - delayTicks;
+
+        }
     }
 
-    public int getDelayTicks() { return delayTicks; }
-    public int getCurrentIndex() { return currentIndex; }
+    public void reset() {
+
+        position = new Position(startPosition.getX(), startPosition.getY());
+        active = false;
+        currentIndex = 0;
+        actionsToReplay.clear();
+        lastPlayerActionCount = 0;
+
+    }
+
+    public boolean isActive() {
+
+        return active;
+    }
+
+    public int getDelayTicks() {
+
+        return delayTicks;
+    }
+
+    public int getCurrentIndex() {
+
+        return currentIndex;
+    }
+
+    public Position getStartPosition() {
+        
+        return startPosition;
+    }
 }
