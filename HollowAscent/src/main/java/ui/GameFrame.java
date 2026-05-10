4f1c860a175ef45a6745507bf975d63d8f44ce22
Flint Harvey Ledesma Geverola <flintharveygg@gmail.com>
2026-05-10 22:21:13 +0800

    feat(GameFrame): rewrite GameFrame to support exclusive fullscreen mode

--- HollowAscent/src/main/java/ui/GameFrame.java
+++ HollowAscent/src/main/java/ui/GameFrame.java
@@ -1,9 +1,31 @@
+package ui;
+
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import javax.swing.JFrame;
+import util.Constants;
+
 public class GameFrame extends JFrame {
+
     public GameFrame() {
-        setTitle(“Hollow Ascent”);
-        setSize(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+
+        setTitle(Constants.GAME_TITLE);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice gd = ge.getDefaultScreenDevice();
+
+        if (gd.isFullScreenSupported()) {
+
+            gd.setFullScreenWindow(this); 
+
+        } else {
+            
+            setExtendedState(JFrame.MAXIMIZED_BOTH);
+        }
+
+        setVisible(true); 
         setResizable(false);
-        setLocationRelativeTo(null);
     }
 }
