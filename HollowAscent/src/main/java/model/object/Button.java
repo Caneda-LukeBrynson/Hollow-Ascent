334d57dab3a3c9ba5b0afeee087a567efdb3c21b
Docena-Shakira <shakiramarie.docena@cit.edu>
2026-05-16 15:32:43 +0800

    fix(Button): update reset() to call door.reset() instead of door.close()

--- HollowAscent/src/main/java/model/object/Button.java
+++ HollowAscent/src/main/java/model/object/Button.java
@@ -1,55 +1,50 @@
-package model.object; 
+package model.object;
 
-import model.Position; 
+import model.Position;
 
-public class Button { 
-    
-    private Position position; 
-    private Door linkedDoor; 
-    private boolean pressed; 
+public class Button {
 
+    private Position position;
+    private Door linkedDoor;
+    private boolean pressed;
 
-    public Button(Position position, Door linkedDoor) { 
-        this.position = position; 
-        this.linkedDoor = linkedDoor; 
-        this.pressed = false; 
-    
-    } 
-    
-    public void onStep() { 
-        
-        pressed = true; 
-        
-        if (linkedDoor != null) linkedDoor.open(); 
-    
-    } 
-    
-    public void reset() { 
-        
-        pressed = false; 
-        
+    public Button(Position position, Door linkedDoor) {
+
+        this.position = position;
+        this.linkedDoor = linkedDoor;
+        this.pressed = false;
+
+    }
+
+    public void onStep() {
+
+        pressed = true;
+
+        if (linkedDoor != null) linkedDoor.open();
+
+    }
+
+    public void reset() {
+
+        pressed = false;
+
         if (linkedDoor != null) 
             
-            linkedDoor.close(); 
-        
-        } 
-        
-        public Position getPosition() { 
-            
-            return position; 
-        
-        } 
-        
-        public boolean isPressed() { 
-            
-            return pressed; 
-        
-        } 
-        
-        public Door getLinkedDoor() { 
-            
-            return linkedDoor; 
-        
-        } 
-    
+            linkedDoor.reset();
     }
+
+    public Position getPosition() {
+
+        return position;
+    }
+
+    public boolean isPressed() {
+
+        return pressed;
+    }
+
+    public Door getLinkedDoor() {
+        
+        return linkedDoor;
+    }
+}
